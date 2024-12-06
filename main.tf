323730b900b0d1fe3e78044810bff5f5c045819f
Shashikant3131 <ghanwatshashikant7@gmail.com>
2024-12-06 16:23:33 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,2 +1,18 @@
-resource "aws_vpc" "my_vpc"{
-cidr ="10.10.0.0/16"
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_vpc" "myvpc" {
+    cidr_block = "20.20.0.0/16"
+  
+}
